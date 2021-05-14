bb28ffc9903a470a3ddcac4e6bc42b9b25eef870
Foxar <mostek3pl@gmail.com>
2021-05-14 11:28:33 +0200

    Created initial SCSS files and style structure, added first classnames to React components.

--- assets/components/NavBar.js
+++ assets/components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
-        <div>
+        <div className="navbar">
             <div><Link to="/">Home</Link></div>
             <div><Link to="/test">Test page</Link></div>
             <div><Link to="/player">Player</Link></div>
--- assets/components/PlayerView/PlayerPage.js
+++ assets/components/PlayerView/PlayerPage.js
@@ -1,11 +1,14 @@
+
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+
 
 class PlayerPage extends Component {
     render() {
         return (
-            <div>
+            <div className="playerPage">
                 <h4>Video playing page</h4>
-                <video></video>
+                <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
             </div>
         );
     }
