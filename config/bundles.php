93487686162bb165be982008baa50c511dd5ed48
Foxar <mostek3pl@gmail.com>
2021-05-18 12:32:36 +0200

    Added Doctrine ORM.

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
