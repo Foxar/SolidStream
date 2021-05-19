229bc4072a2a973f7443e416af808edd9e82ac04
Foxar <mostek3pl@gmail.com>
2021-05-19 20:12:51 +0200

    Installed Monolog to clean up logging on standard-output, and allow for more robust logging systems.

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
