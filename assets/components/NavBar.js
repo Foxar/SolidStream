95c85bcded1bc408bf5276f43be9168860efd75b
Foxar <mostek3pl@gmail.com>
2021-05-21 11:46:16 +0200

    Modified navbar to display current user's username.

--- assets/components/NavBar.js
+++ assets/components/NavBar.js
@@ -1,15 +1,22 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 export default function NavBar() {
     return (
         <div className="navbar">
-            <div><Link to="/">Home</Link></div>
-            <div><Link to="/test">Test page</Link></div>
-            <div><Link to="/player">Player</Link></div>
-            <div><Link to="/login">Login</Link></div>
-            <div><Link to="/streamingpanel">Streaming</Link></div>
+            <div className="links">
+                <div><Link to="/">Home</Link></div>
+                <div><Link to="/test">Test page</Link></div>
+                <div><Link to="/player">Player</Link></div>
+                <div><Link to="/login">Login</Link></div>
+                <div><Link to="/streamingpanel">Streaming</Link></div>
+            </div>
+            <div className="profile">
+                <p>Logged in as:</p>
+                <p className="loggedInAs">{Cookies.get('username')}</p>
+            </div>
         </div>
     );
 }
