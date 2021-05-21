9018f159c00079bfc5a13c6d996504bb438c5ae8
Foxar <mostek3pl@gmail.com>
2021-05-21 10:26:14 +0200

    Changed: Errors in streamingpanel now disappear after some time.

--- assets/components/Streaming/StreamingPanel/StreamPanel.js
+++ assets/components/Streaming/StreamingPanel/StreamPanel.js
@@ -7,11 +7,36 @@ class StreamPanel extends Component {
         super();
         this.state = {
             streamID: null,
+            infoMessage: '',
             errorMessage: '',
             deleteMessage: ''
         }
         this.handleStartStream = this.handleStartStream.bind(this);
         this.handleStopStream = this.handleStopStream.bind(this);
+        this.setTimedError = this.setTimedError.bind(this);
+        this.setTimedInfo = this.setTimedInfo.bind(this);
+        this.setTimedDeleteNotice = this.setTimedDeleteNotice.bind(this);
+    }
+
+    setTimedError(msg, time = 5000) {
+        this.setState({ errorMessage: msg });
+        setTimeout(() => {
+            this.setState({ errorMessage: "" });
+        }, time);
+    }
+
+    setTimedInfo(msg, time = 5000) {
+        this.setState({ infoMessage: msg });
+        setTimeout(() => {
+            this.setState({ infoMessage: "" });
+        }, time);
+    }
+
+    setTimedDeleteNotice(msg, time = 5000) {
+        this.setState({ deleteMessage: msg });
+        setTimeout(() => {
+            this.setState({ deleteMessage: "" });
+        }, time);
     }
 
     handleStartStream() {
@@ -26,7 +51,10 @@ class StreamPanel extends Component {
             if (response.ok) {
                 this.setState({ errorMessage: "" });
                 response.json().then(
-                    data => { this.setState({ streamID: data.id }); }
+                    data => {
+                        this.setState({ streamID: data.id });
+                        this.setTimedInfo("Your stream can be started!");
+                    }
                 );
             }
             else if (response.status == 401) {
@@ -49,12 +77,12 @@ class StreamPanel extends Component {
         responsePromise.then(response => {
             if (response.ok) {
                 response.json().then(
-                    this.setState({
-                        deleteMessage: 'Stream stopped!'
-                    })
+                    this.setTimedDeleteNotice("Stream stopped!")
                 );
+            } else if (response.status == 404) {
+                this.setTimedError("Stream did not yet start.");
             } else {
-                this.setState({ errorMessage: "Unknown error!" });
+                this.setTimedError("Unknown error!");
             }
         });
     }
@@ -64,10 +92,10 @@ class StreamPanel extends Component {
             <div className="streamingPanel">
                 {this.state.errorMessage && (<p className="errorMessage">{this.state.errorMessage}</p>)}
                 {this.state.deleteMessage && (<p className="deleteMessage">{this.state.deleteMessage}</p>)}
+                {this.state.infoMessage && (<p className="infoMessage">{this.state.infoMessage}</p>)}
                 {this.state.streamID &&
                     (
                         <div className="streamingKey">
-                            <p>Your stream can be started!</p>
                             <p>Your key: {this.state.streamID}</p>
                         </div>
                     )}
