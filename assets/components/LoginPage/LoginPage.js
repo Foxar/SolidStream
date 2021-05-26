d4b8a4c7641cb2c41f9909bacd6755cba80cf3ed
Foxar <mostek3pl@gmail.com>
2021-05-26 12:13:22 +0200

    Moved large login logic into separate standalone js file in new directory structure 'services'.

--- assets/components/LoginPage/LoginPage.js
+++ assets/components/LoginPage/LoginPage.js
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Collapse from '@material-ui/core/Collapse';
+import { handleLogin } from '../../services/handleLogin';
 
 
 
@@ -23,6 +24,7 @@ class LoginPage extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.saveError = this.saveError.bind(this);
         this.setTimedError = this.setTimedError.bind(this);
+        this.handleLogin = handleLogin.bind(this);
     }
 
     handleUserChange(e) {
@@ -59,64 +61,9 @@ class LoginPage extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
-
-        //Submit login data to backend, save the JWT token and save as a cookie to pass on 
-        //with each next api call.
-        const data = {
-            "username": this.state.username,
-            "password": this.state.password
-        };
-        const responsePromise = fetch('http://localhost:8000/api/login_check', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        responsePromise.then(response => {
-            //If the login_check response is OK, save the JWT token:
-            if (response.ok) {
-                response.json().then(
-                    data => {
-                        //Save the JWT token to a cookie, clear error messages.
-                        Cookies.set('jwt-token', data.token);
-                        this.saveError("");
-
-
-                        //Fetch user entity of the user that has logged in.
-                        fetch('http://localhost:8000/secureapi/currentUser', {
-                            method: 'GET',
-                            headers: {
-                                'Authorization': 'BEARER '.concat(Cookies.get('jwt-token'))
-                            }
-                        })
-                            .then(response => {
-                                if (response.ok) {
-                                    response.json().then(
-                                        data => {
-                                            Cookies.set('username', data.username);
-                                            Cookies.set('userid', data.id);
-                                            window.location.assign("/");
-                                        }
-                                    );
-                                } else {
-                                    this.setTimedError("Unknown error!");
-                                }
-                            });
-                    }
-                );
-            } else {
-                //Otherwise, display error message.
-                if (response.status == 401) {
-                    this.setTimedError("Invalid credentials!");
-                }
-                else
-                    this.setTimedError("Unknown error!");
-            }
-        });
+        console.log(this);
+        this.handleLogin();
     }
-
-
 
     render() {
         return (
