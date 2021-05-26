b865fb7330d9eea62986ab7c561236a8cd5afe9a
Foxar <mostek3pl@gmail.com>
2021-05-26 11:21:07 +0200

    Added typography and avatars to the stream player chat.

--- assets/components/Streaming/StreamChat/ChatMessage.js
+++ assets/components/Streaming/StreamChat/ChatMessage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
 
 export default function ChatMessage(props) {
     return (
         <div className="chatMessage">
-            <Typography variant='subtitle2'>{props.name}</Typography>
-            <Typography variant='subtitle1'>{props.message}</Typography>
+            <Avatar src={`https://i.pravatar.cc/150?u=${props.name}`} />
+            <div className="content">
+                <Typography variant='subtitle2'>{props.name}</Typography>
+                <Typography variant='subtitle1'>{props.message}</Typography>
+            </div>
         </div>
     );
 }
